6e15ebe811ed1a4fb558b627c3f4e6e88d7a2e69
linthacus <linthacus@gmail.com>
2017-05-28 08:44:36 +0100

    ok

--- King-Fisher/King-Fisher/Encryptor.h
+++ King-Fisher/King-Fisher/Encryptor.h
@@ -4,7 +4,32 @@
 #ifndef _KGFR_ENCRYPTION_
 #define _KGFR_ENCRYPTION_
 
-class FUISEncryption {
+class FUISMaths {
+public:
+	long int ChkCoprime(long int seconds, long int address) {
+		bool Coprime = (address<seconds) ? ChkCoprime(seconds, address) : !(address%seconds) ? (seconds == 1) : ChkCoprime(seconds, address%seconds);
+		if (Coprime != true) {
+			seconds++;
+			ChkCoprime(seconds,address);
+		}
+		if (Coprime == false) {
+			return seconds, address;
+		};
+	};
+	int factorial(int x, int result = 1) {
+		if (x == 1) return result; else return factorial(x - 1, x * result);
+	}
+protected:
+
+
+
+
+
+};
+
+
+
+class FUISEncryption: public FUISMaths {
 public:
 	void encrypt(std::string memLoc) {
 
@@ -12,9 +37,6 @@ public:
 		seconds = time(NULL);
 
 		std::string binarytime = std::bitset<64>(seconds).to_string(); //to binary
-		
-
-
 		std::string binaryaddress = std::bitset<64>(seconds).to_string(); //to binary
 	
 	
